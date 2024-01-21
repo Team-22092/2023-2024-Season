ac2305a34ed8d5af975d0d60a3f36229abfb7e8d
Aaron <aaronz27@g.eips.ca>
2024-01-21 11:48:35 -0700

    commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/testAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/testAuto.java
@@ -1,6 +1,6 @@
 package org.firstinspires.ftc.teamcode.Auto;
 
-import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.drive.DriveConstants;
